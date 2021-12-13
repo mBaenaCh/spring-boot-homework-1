71b2d29dd2236750b7d0b9b0e5121dac02a2421e
mBaenaCh <mateo.baena@hotmail.com>
2021-12-13 16:10:25 -0500

    Feature: Setting up the controller

---
+++ src/main/java/com/mateo/cars/controller/CarsController.java
@@ -0,0 +1,46 @@
+package com.mateo.cars.controller;
+import com.mateo.cars.domain.Car;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/* Con nuestro controller definiremos las funcionalidades que actuaran sobre nuestor modelo del dominio
+*
+*  Dado que nuestro sistema es una API REST, vamos a exponer estas funcionalidades por medio de rutas y
+*  metodos HTTP
+*
+*  Con Springboot, usaremos la depedencia "RestController" para dotar de esas funcionalidades a nuestra clase*/
+
+@RestController
+@RequestMapping //Nos permitira modelar toda la funcionalidad alrededor de una misma ruta
+public class CarsController {
+
+    @GetMapping
+    public List<Car> getCars(){
+        return List.of();
+    }
+
+    @GetMapping(value = "/{id}")
+    //La dependencia PathVariable nos permite manejar los parametros que son enviados junto a nuestra ruta
+    public Car getCarById(@PathVariable String id){
+        return new Car();
+    }
+
+    @PutMapping
+    /*La dependencia de RequestBody nos permite recibir un JSON que sera convertido al formato especificado
+      en el parametro de entrada*/
+    public Car createCar(@RequestBody Car car){
+        return car;
+    }
+
+    @PostMapping(value="/{id}")
+    public Car updateCarById(@PathVariable String id, @RequestBody Car car){
+        return car;
+    }
+
+    @DeleteMapping(value="/{id}")
+    public void deleteCarById(@PathVariable String id){
+        
+    }
+
+}
