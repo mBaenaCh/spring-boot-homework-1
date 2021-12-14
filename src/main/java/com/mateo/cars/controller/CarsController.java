908036c775ce5debcf6c06f65975e8a775a71b8f
mBaenaCh <mateo.baena@hotmail.com>
2021-12-13 23:11:19 -0500

    Feature: Interface methods implementation

--- src/main/java/com/mateo/cars/controller/CarsController.java
+++ src/main/java/com/mateo/cars/controller/CarsController.java
@@ -1,6 +1,7 @@
 package com.mateo.cars.controller;
 
 import com.mateo.cars.domain.Car;
+import com.mateo.cars.repository.CarsRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,36 +18,54 @@ import java.util.List;
    metodos HTTP */
 @RequestMapping(value="/cars")
 public class CarsController {
-    
 
-    @GetMapping
-    public List<Car> getCars(){
-        //return List.of();
-        return null;
-    }
+    /*
+    * Controller -> Alto nivel -> Abstraccion generalizada alrededor de varias funcionalidades
+    *
+    * Repository -> Bajo nivel -> Abstraccion mas especifica que define como se hara cada
+    *                             funcionalidad necesaria
+    *
+    * Con el principio de inversion de dependencia buscamos que este primer modulo no dependa del segundo (Reducimos acoplamiento)
+    * */
 
-    @GetMapping(value = "/{id}")
-    //La anotacion PathVariable nos permite manejar los parametros que son enviados junto a nuestra ruta
-    public Car getCarById(@PathVariable String id){
-        return new Car();
+    private CarsRepository carsRepository; //Instanciacion que nos permitira usar los metodos de la interfaz
+
+    public CarsController(CarsRepository carsRepository) {
+        this.carsRepository = carsRepository;
     }
 
     @PutMapping
     /*La anotacion RequestBody nos permite recibir un JSON que sera convertido al formato especificado
       en el parametro de entrada*/
-    public void createCar(@RequestBody Car car){
-        car = new Car();
+    public void createCar(
+            @RequestBody Car car){
+        carsRepository.createCar(car);
+    }
+
+    @GetMapping
+    public List<Car> getCars(){
+        return carsRepository.getAllCars();
+    }
+
+    @GetMapping(value = "/{id}")
+    //La anotacion PathVariable nos permite manejar los parametros que son enviados junto a nuestra ruta
+    public Car getCarById(
+            @PathVariable String id){
+        return carsRepository.getCarById(id);
     }
 
     @PostMapping(value="/{id}")
     /* Podremos combinar el uso de distintas anotaciones en los parametros de nuestros metodos*/
-    public void updateCarById(@PathVariable String id, @RequestBody Car car){
-        car = new Car();
+    public void updateCarById(
+            @PathVariable String id,
+            @RequestBody Car car){
+        carsRepository.updateCarById(id, car);
     }
 
     @DeleteMapping(value="/{id}")
-    public void deleteCarById(@PathVariable String id){
-        
+    public void deleteCarById(
+            @PathVariable String id){
+        carsRepository.deleteCarById(id);
     }
 
 }
