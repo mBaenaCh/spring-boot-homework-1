184f86606135b459bec12d0a79e19eb9087bd456
mBaenaCh <mateo.baena@hotmail.com>
2021-12-15 16:50:46 -0500

    Feature: Car model class testing

--- src/test/java/com/mateo/cars/domain/CarModelTest.java
+++ src/test/java/com/mateo/cars/domain/CarModelTest.java
@@ -29,7 +29,32 @@ class CarModelTest {
         Executable executable = () -> new CarModel(value);
 
         //Assert
-        assertThrows(NullPointerException.class, executable);
+        assertThrows(IllegalArgumentException.class, executable);
 
+    }
+
+    @Test
+    public void mustReturnIllegalArgumentExceptionWhenValueCharsAreGreaterThan80(){
+        //Arrange
+        String value = "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel";
+
+        //Act
+        Executable executable = () -> new CarModel(value);
+
+        //Assert
+        assertThrows(IllegalArgumentException.class, executable);
+
+    }
+
+    @Test
+    public void mustReturnIllegalArgumentExceptionWhenValueDoesntComplyTheRegex(){
+        //Arrange
+        String value = "Nam quis nulla. Integer malesuada. In 4#";
+
+        //Act
+        Executable executable = () -> new CarModel(value);
+
+        //Assert
+        assertThrows(IllegalArgumentException.class, executable);
     }
 }
