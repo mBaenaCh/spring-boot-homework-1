d56d378847107ea35b64b44ff1c7e87cd5d66b6c
mBaenaCh <mateo.baena@hotmail.com>
2021-12-16 21:01:41 -0500

    feature: Car Model parser

--- src/main/java/com/mateo/cars/configuration/jackson/codecs/CarModelParser.java
+++ src/main/java/com/mateo/cars/configuration/jackson/codecs/CarModelParser.java
@@ -21,5 +21,33 @@ package com.mateo.cars.configuration.jackson.codecs;
 *
 * }*/
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.mateo.cars.domain.CarModel;
+
+import java.io.IOException;
+
 public class CarModelParser {
+
+    public static class Serialize extends JsonSerializer<CarModel>{
+
+        @Override
+        public void serialize(CarModel value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.toString());
+        }
+    }
+
+    public static class Deserialize extends JsonDeserializer<CarModel>{
+
+        @Override
+        public CarModel deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+            return new CarModel(p.getValueAsString());
+        }
+    }
+
 }
