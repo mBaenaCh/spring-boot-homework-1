6d1cd2a26694b634f3ebfa218a67b8066a07d0a0
mBaenaCh <mateo.baena@hotmail.com>
2021-12-17 00:27:48 -0500

    Feature: model definition

--- src/main/java/com/mateo/cars/model/CreateCarOutput.java
+++ src/main/java/com/mateo/cars/model/CreateCarOutput.java
@@ -1,4 +1,24 @@
 package com.mateo.cars.model;
 
+import com.mateo.cars.domain.Car;
+
 public class CreateCarOutput {
+
+    private Car car;
+
+    public CreateCarOutput(){
+
+    }
+
+    public CreateCarOutput(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
--- src/main/java/com/mateo/cars/model/UpdateCarByIdOutput.java
+++ src/main/java/com/mateo/cars/model/UpdateCarByIdOutput.java
@@ -1,4 +1,23 @@
 package com.mateo.cars.model;
 
+import com.mateo.cars.domain.Car;
+
 public class UpdateCarByIdOutput {
+
+    private Car car;
+
+    public UpdateCarByIdOutput() {
+    }
+
+    public UpdateCarByIdOutput(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
